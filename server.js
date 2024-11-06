939a16b5eaf1dd10a33e33856467f1b8c082938a
andrewindeche <indecheandrew@gmail.com>
2024-11-06 13:05:54 +0300

    remove unused variables

--- server.js
+++ server.js
@@ -5,24 +5,20 @@ const cors = require('cors');
 const connectDB = require('./server/db');
 const salariesRouter = require('./server/salaries');
 
-let envelopes = [];
-let totalBudget = 0;
-
 const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(cors());
 app.use(bodyParser.json());
+
 app.use('/api/envelopes',salariesRouter)
 
-
+app.get('/',(req,res) => {
+  res.status(200).send("welcome to MongoDB")
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`)
   });
-
-app.get('/',(req,res) => {
-    res.status(200).send(`Your budgets are:${ envelopes, totalBudget }`);
-});
 
 module.exports = app;
--- server/salaries.js
+++ server/salaries.js
@@ -5,20 +5,29 @@ let monthlySalary = 150000;
 let envelopes = [];
 let totalBudget = 0;
 
-salariesRouter.post('/api/envelopes',(req,res) => {
+salariesRouter.post('/',(req,res) => {
     const {name,budget} = req.body;
     const proposedBudget = totalBudget + budget;
     switch(true){
         case proposedBudget > monthlySalary:
             return res.status(400).send("your budget has exceeded your salary");
-        case !name || !budget || !NaN(budget):
+        case !name || !budget:
             return res.status(400).send("Invalid data");
         default: 
             const newEnvelope = { id: envelopes.length + 1, name, budget};
             envelopes.push(newEnvelope);
             totalBudget = proposedBudget;
-            return res.status(201).send
+            return res.status(201).send(newEnvelope);
     };
 });
+
+salariesRouter.get('/', (req, res) => {
+    const balance = monthlySalary - totalBudget;
+    res.status(200).json({
+        balance: balance,
+        envelopes: envelopes
+    });
+});
+
 
 module.exports = salariesRouter;
